608c894e53a93305269527769ddc2a74b4c940be
Johan Bos <johan@Johans-MacBook.local>
2018-03-10 16:59:49 +0100

    Added way to bring up the selection menu again by long pressing the action bar

--- AppConfigExample/src/main/java/com/crescentflare/appconfigexample/MainActivity.java
+++ AppConfigExample/src/main/java/com/crescentflare/appconfigexample/MainActivity.java
@@ -3,6 +3,9 @@ package com.crescentflare.appconfigexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.crescentflare.appconfig.activity.ManageAppConfigActivity;
@@ -98,5 +101,62 @@ public class MainActivity extends AppCompatActivity implements AppConfigStorage.
         tvConfigConsoleEnabled.setText(getString(R.string.prefix_config_console_enabled) + " " + (ExampleAppConfigManager.currentConfig().isConsoleEnabled() ? "true" : "false"));
         tvConfigConsoleTimeout.setText(getString(R.string.prefix_config_console_timeout_sec) + " " + ExampleAppConfigManager.currentConfig().getConsoleTimeoutSec());
         tvConfigLogLevel.setText(getString(R.string.prefix_config_log_level) + " " + ExampleAppConfigManager.currentConfig().getLogLevel().toString());
+
+        // Set long click listener on the action bar to show the selection menu again
+        View actionBar = findActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    ManageAppConfigActivity.startWithResult(MainActivity.this, RESULT_CODE_MANAGE_APP_CONFIG);
+                    return true;
+                }
+            });
+        }
+    }
+
+
+    // ---
+    // Helpers
+    // ---
+
+    public ViewGroup findActionBar()
+    {
+        int id = getResources().getIdentifier("action_bar", "id", "android");
+        ViewGroup actionBar = null;
+        if (id != 0)
+        {
+            actionBar = (ViewGroup)findViewById(id);
+        }
+        if (actionBar == null)
+        {
+            actionBar = findToolbar((ViewGroup)findViewById(android.R.id.content).getRootView());
+        }
+        return actionBar;
+    }
+
+    private ViewGroup findToolbar(ViewGroup viewGroup)
+    {
+        ViewGroup toolbar = null;
+        for (int i = 0, len = viewGroup.getChildCount(); i < len; i++)
+        {
+            View view = viewGroup.getChildAt(i);
+            if (view.getClass().getName().equals("android.support.v7.widget.Toolbar") || view.getClass().getName().equals("android.widget.Toolbar"))
+            {
+                toolbar = (ViewGroup)view;
+            }
+            else if (view instanceof ViewGroup)
+            {
+                toolbar = findToolbar((ViewGroup)view);
+            }
+            if (toolbar != null)
+            {
+                break;
+            }
+        }
+        return toolbar;
     }
 }
